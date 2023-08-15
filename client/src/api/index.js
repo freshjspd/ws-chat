144e6514bf5067c971195c62f906b09a9276c932
Viktoriia <pecourses>
2023-08-16 01:51:21 +0300

    Add server emitting

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -2,3 +2,18 @@ import { io } from 'socket.io-client';
 
 // генерируем событие 'connection' на сервере
 const socket = io('http://localhost:5000');
+
+// socket.on('event', (payload) => {}); - подписка на событие
+// socket.emit('event', payload) - генерация события (на сервер)
+
+socket.on('EVENT_FOR_ALL', payload => {
+  console.log('payload :>> ', payload);
+});
+
+socket.on('EVENT_FOR_SELF', payload => {
+  console.log('payload for self:>> ', payload);
+});
+
+socket.on('EVENT_FOR_OTHER', payload => {
+  console.log('payload for other:>> ', payload);
+});
--- server/index.js
+++ server/index.js
@@ -11,11 +11,20 @@ const io = new Server(httpServer, {
   cors: { origin: 'http://localhost:3000' },
 });
 
-// io.on('event' ()=>{}) ~ btn.addEventListener('click',()=>{}) - подписка на событие
-// io.emit('event', payload) ~ dispatch(createUserThunk(newUser)) - генерация события - для всех
+// io.on('event' (payload)=>{}) ~ btn.addEventListener('click',()=>{})
+//      - подписка на событие
+// io.emit('event', payload) ~ dispatch(createUserThunk(newUser))
+//      - генерация события - для всех клиентов
+// socket.emit('event', payload)
+//      - генерация события для себя
+// socket.broadcast.emit('event', payload)
+//      - генерация события всех кроме для себя
 
 io.on('connection', socket => {
+  console.log('Connection with socket are established');
   io.emit('EVENT_FOR_ALL', 'Hello for everyone from server)');
+  socket.emit('EVENT_FOR_SELF', 'Hello, you connection is established');
+  socket.broadcast.emit('EVENT_FOR_OTHER', 'New socket is connected');
 });
 
 httpServer.listen(PORT, () =>
